f6401e246ff91b17616afff60c8be140f47de4f1
marathonman4202 <roy.haggerty@oregonstate.edu>
2015-02-13 08:04:16 -0800

    found error in delimiter

--- ptrack.py
+++ ptrack.py
@@ -8,6 +8,6 @@ import numpy as np
 path = ''
 data_file = path + 'data_ex.txt'
 
-data = np.genfromtxt(data_file, delimiter=',',skip_header=1) # Read  file
+data = np.genfromtxt(data_file, skip_header=1) # Read  file.  Other delimiters as delimiter = ',' or whatever
 
 assert False
